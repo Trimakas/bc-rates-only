b84aa92575ef02f5a6d97903c463ac94b11da9d6
dmkonst <warqueq@gmail.com>
2019-08-21 22:29:10 +0300

    fix puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -29,3 +29,4 @@ on_worker_boot do
     require "active_record"
     ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
     ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+end
